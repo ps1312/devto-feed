0b8b58d323532b36035b9c18763c14c8bd4a2575
Paulo Sérgio <ps131296@gmail.com>
2021-03-13 23:08:09 -0300

    add axios module

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,6 +26,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
+    '@nuxtjs/axios',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
