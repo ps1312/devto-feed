68aac86c073b41f1419cbc0f60c113e1983d5cd4
Paulo Sérgio <ps131296@gmail.com>
2021-03-14 10:57:50 -0300

    extracted navbar to its own component

---
+++ components/BaseNavBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav
+    class="fixed top-0 h-20 w-screen bg-white flex items-center shadow-md bg-indigo-500"
+  >
+    <a
+      class="ml-8 hover:bg-indigo-600 px-4 py-3 rounded-md cursor-pointer"
+      @click.prevent="scrollToTop()"
+    >
+      <span class="text-4xl font-bold text-white border-b-2 border-white">
+        dev.to feed
+      </span>
+    </a>
+  </nav>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'BaseNavBar',
+  methods: {
+    scrollToTop() {
+      window.scrollTo(0, 0)
+    },
+  },
+})
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,34 +1,21 @@
 <template>
   <div>
-    <div
-      class="fixed top-0 h-20 w-screen bg-white flex items-center shadow-md bg-indigo-500"
-    >
-      <a
-        class="ml-8 hover:bg-indigo-600 px-4 py-3 rounded-md cursor-pointer"
-        @click.prevent="scrollToTop()"
-      >
-        <span class="text-4xl font-bold text-white border-b-2 border-white">
-          dev.to feed
-        </span>
-      </a>
-    </div>
-    <div class="">
-      <div class="pt-28 lg:max-w-4xl mx-auto">
-        <Nuxt />
-      </div>
+    <base-nav-bar />
+
+    <div class="pt-28 lg:max-w-4xl mx-auto">
+      <Nuxt />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import BaseNavBar from '~/components/BaseNavBar.vue'
 
 export default Vue.extend({
   name: 'DefaultLayout',
-  methods: {
-    scrollToTop() {
-      window.scrollTo(0, 0)
-    },
+  components: {
+    BaseNavBar,
   },
 })
 </script>
