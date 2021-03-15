131c100d657e95100774378aa3cc1700c39cddee
Paulo Sérgio <ps131296@gmail.com>
2021-03-14 21:55:00 -0300

    add more test cases and extracted a helper method

--- test/DragnDrop.spec.js
+++ test/DragnDrop.spec.js
@@ -3,32 +3,43 @@ import DragnDrop from '@/components/DragnDrop.vue'
 
 describe('DragnDrop.vue', () => {
   it('reorders list on drag and drop events', async () => {
+    let wrappersList
     const wrapper = mount(DragnDrop)
-    const wrappersList = await wrapper.findAll('li').wrappers
 
-    const dataTransferMock = {
-      data: null,
-
-      setData(_type, from) {
-        this.data = from
-      },
-
-      getData() {
-        return this.data
-      },
-    }
-
-    await wrappersList[2].trigger('dragstart', {
-      dataTransfer: dataTransferMock,
-    })
-
-    const firstEl = wrappersList[0]
-    await firstEl.trigger('dragover')
-
-    await firstEl.trigger('drop', {
-      dataTransfer: dataTransferMock,
-    })
-
+    wrappersList = await wrapper.findAll('li').wrappers
+    await simulate(wrappersList[2], 'dragstart')
+    await simulate(wrappersList[0], 'dragover')
+    await simulate(wrappersList[0], 'drop')
     expect(wrapper.vm.items).toStrictEqual([2, 1, 0])
+
+    wrappersList = await wrapper.findAll('li').wrappers
+    await simulate(wrappersList[1], 'dragstart')
+    await simulate(wrappersList[0], 'dragover')
+    await simulate(wrappersList[0], 'drop')
+    expect(wrapper.vm.items).toStrictEqual([1, 2, 0])
+
+    wrappersList = await wrapper.findAll('li').wrappers
+    await simulate(wrappersList[0], 'dragstart')
+    await simulate(wrappersList[2], 'dragover')
+    await simulate(wrappersList[2], 'drop')
+    expect(wrapper.vm.items).toStrictEqual([0, 2, 1])
   })
+
+  const dataTransferMock = {
+    data: null,
+
+    setData(_type, from) {
+      this.data = from
+    },
+
+    getData() {
+      return this.data
+    },
+  }
+
+  async function simulate(el, event) {
+    await el.trigger(event, {
+      dataTransfer: dataTransferMock,
+    })
+  }
 })
