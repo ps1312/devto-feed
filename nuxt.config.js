b44f16115d537d92e5416ce9b7566860eb1160e2
Paulo Sérgio <ps131296@gmail.com>
2021-03-15 00:08:10 -0300

    changed favicon and title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'my-devto',
+    title: 'devto-feed',
     htmlAttrs: {
       lang: 'en',
     },
